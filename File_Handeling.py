c96ae5562a8bff258dace24c8d6d182efe94d72c
Sanithu <SanithuHasmalKudaliyana@gmail.com>
2025-11-24 08:41:16 +0530

    Add files via upload

---
+++ File_Handeling.py
@@ -0,0 +1,18 @@
+##with open('shopping.txt', 'w') as f:
+##    f.write("Bread 1 1.39\nTomatoes 6 0.26\nMilk 3 1.45")
+##    f.close()
+##This will creat a text file with the name shopping and it will save the items one per line
+
+##totals = {}
+##with open('shopping.txt', 'r') as file:
+##    lines = file.readlines()
+##    for line in lines:
+##        parts = line.strip().split()
+##        item, qty, price = parts[0], int(parts[1]), float(parts[2])
+##        total = qty * price
+##        totals[item] = total
+##
+##for item, total in totals.items():
+##    print(f"Item {item} = total {total:.2f}")
+    #The "Item {item} = total" 
+
